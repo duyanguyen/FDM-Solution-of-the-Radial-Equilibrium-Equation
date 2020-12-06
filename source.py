4bd39222ad0bea1a3f6da1c90b4ff56d8d807ea6
duyanguyen <74983351+duyanguyen@users.noreply.github.com>
2020-12-06 15:49:56 -0500

    Update source.py

--- source.py
+++ source.py
@@ -1,29 +1,31 @@
 #Duy Anh Nguyen - 260745838
-#MECH 535 - Project Fall 2020
+#MECH 535 - Fall 2020 - Project
 
 import math
 from array import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Assumption (an abitrary value of blade width does not affect the result)
+#Assumption
 gamma       = 1.4
+e_gamma     = gamma/(gamma-1)
 R           = 287 #J/kg.K
 C_p         = 1005 #J/kg.K
-T           = 298 #K
-blade_width = 0.04 #m
 
 #Given
+blade_width = 0.1 #m
 N           = (6000*2*math.pi)/(60) #rev/s
 C_x         = 175 #m/s
+T           = 298 #K
 p           = 100000 #Pa
 d_tip       = 1.0 #m
-r_tip       = d_tip/2
 d_hub       = 0.9 #m
+r_tip       = d_tip/2
 r_hub       = d_hub/2
 rho         = p/(R*T) #kg/m3
 m_dot       = rho*C_x*math.pi*(r_tip**2-r_hub**2) #kg/s
 T0          = T+C_x**2/(2*C_p)
+p0          = p/((T/T0)**e_gamma)
 beta1_tip   = 60*2*math.pi/360 #rad
 beta2_tip   = 30*2*math.pi/360 #rad
 beta1_hub   = 55.15*2*math.pi/360 #rad
@@ -31,7 +33,7 @@ beta2_hub   = 8.64*2*math.pi/360 #rad
 loss_total  = 0.05
 
 #Generate a grid with equal spacing in x and r directions (divide blade width to n equal portion)
-n           = 4
+n           = 10
 grid_x_dim  = blade_width * 5
 grid_r_dim  = r_tip - r_hub
 element_dim = blade_width/n
@@ -84,52 +86,32 @@ def psi_cal(i, j):
     psi_result = (r_cal(i, j)**2-r_hub**2)/(r_tip**2-r_hub**2)
     return psi_result
 
-#Initialize loss coefficient, alpha, beta angle and psi distribution matrix
+#Initialize loss coefficient, alpha, beta angle and psi distribution matrices
 loss_grid   = [[loss_cal(i, j) for i in range(x_size)] for j in range(r_size)]
 alpha_grid  = [[0 for i in range(x_size)] for j in range(r_size)]
 beta_grid   = [[beta_cal(i, j) for i in range(x_size)] for j in range(r_size)]
 psi_grid    = [[psi_cal(i, j) for i in range(x_size)] for j in range(r_size)]
 
-#Initialize velocity C_x matrix with special cases at the bottom and top edges of the grid
+#Initialize velocity C_x, C_r and C_m matrices with special cases at the edges of the grid
 C_x_grid = [[0 for i in range(x_size)] for j in range(r_size)]
-for i in x_pos:
-    for j in r_pos:
-        if j == 0:
-            C_x_grid[j][i] =  m_dot*(psi_grid[j+1][i]-psi_grid[j][i])/(2*math.pi*rho*r_cal(i, j)*element_dim)
-        elif j == max(r_pos):
-            C_x_grid[j][i] =  m_dot*(psi_grid[j][i]-psi_grid[j-1][i])/(2*math.pi*rho*r_cal(i, j)*element_dim)
-        else:
-            C_x_grid[j][i] =  m_dot*(psi_grid[j+1][i]-psi_grid[j-1][i])/(2*math.pi*rho*r_cal(i, j)*2*element_dim)
-
-#Initialize velocity C_r matrix with special cases at the left and right edges of the grid
 C_r_grid = [[0 for i in range(x_size)] for j in range(r_size)]
 for i in x_pos:
     for j in r_pos:
+        k, l, m, n = [1, 1, 1, 1]
+        o, q       = [0, 0]
+        if j == 0:
+            n, q = [0, 1]
+        elif j == max(r_pos):
+            m, q = [0, 1]
         if i == 0:
-            C_r_grid[j][i] =  -m_dot*(psi_grid[j][i+1]-psi_grid[j][i])/(2*math.pi*rho*r_cal(i, j)*element_dim)
+            l, o = [0, 1]
         elif i == max(x_pos):
-            C_r_grid[j][i] =  -m_dot*(psi_grid[j][i]-psi_grid[j][i-1])/(2*math.pi*rho*r_cal(i, j)*element_dim)
-        else:
-            C_r_grid[j][i] =  -m_dot*(psi_grid[j][i+1]-psi_grid[j][i-1])/(2*math.pi*rho*r_cal(i, j)*2*element_dim)
+            k, o = [0, 1]
 
-#Initialize velocity C_m matrix
+        C_r_grid[j][i] = -m_dot*(psi_grid[j][i+1*k]-psi_grid[j][i-1*l])/(2*math.pi*rho*r_cal(i, j)*(2-o)*element_dim)
+        C_x_grid[j][i] = m_dot*(psi_grid[j+1*m][i]-psi_grid[j-1*n][i])/(2*math.pi*rho*r_cal(i, j)*(2-q)*element_dim)
+
 C_m_grid = [[(C_x_grid[j][i]**2+C_r_grid[j][i]**2)**(1/2) for i in range(x_size)] for j in range(r_size)]
-
-#Initialize all other parameters grid for calculation block
-T_grid          = [[T0 for i in range(x_size)] for j in range(r_size)]
-vorticity_grid  = [[0 for i in range(x_size)] for j in range(r_size)]
-V_theta_grid    = [[N*r_cal(i, j) for i in range(x_size)] for j in range(r_size)]
-V_sqrt_grid     = [[0 for i in range(x_size)] for j in range(r_size)]
-C_sqrt_grid     = [[0 for i in range(x_size)] for j in range(r_size)]
-h0_grid         = [[C_p*T0 for i in range(x_size)] for j in range(r_size)]
-h0r_grid        = [[C_p*T0 for i in range(x_size)] for j in range(r_size)]
-h_grid          = [[h0r_grid[j][i]-V_sqrt_grid[j][i]/2 for i in range(x_size)] for j in range(r_size)]
-p0_grid         = [[p for i in range(x_size)] for j in range(r_size)]
-p0r_grid        = [[p for i in range(x_size)] for j in range(r_size)]
-p0r_ideal_grid  = [[p for i in range(x_size)] for j in range(r_size)]
-p_grid          = [[p0_grid[j][i]*(h_grid[j][i]/h0_grid[j][i])**(gamma/(gamma-1)) for i in range(x_size)] for j in range(r_size)]
-rho_grid        = [[rho for i in range(x_size)] for j in range(r_size)]
-s_grid          = [[0 for i in range(x_size)] for j in range(r_size)]
 
 #Starting swirl inside the rotor
 C_theta_grid  = [[0 for i in range(x_size)] for j in range(r_size)]
@@ -140,15 +122,34 @@ for i in x_pos:
             C_theta_grid[j][i]  = N*r_cal(i, j)-C_m_grid[j][i]*math.tan(beta_grid[j][i])
             rC_theta_grid[j][i] = r_cal(i, j)*C_theta_grid[j][i]
 
-for run in range(21):
+#Initialize all other parameters grid for calculation block
+T_grid          = [[T                                                   for i in range(x_size)] for j in range(r_size)]
+vorticity_grid  = [[0                                                   for i in range(x_size)] for j in range(r_size)]
+V_theta_grid    = [[N*r_cal(i, j)-C_theta_grid[j][i]                    for i in range(x_size)] for j in range(r_size)]
+V_sqrt_grid     = [[C_m_grid[j][i]**2+V_theta_grid[j][i]**2             for i in range(x_size)] for j in range(r_size)]
+C_sqrt_grid     = [[C_m_grid[j][i]**2+C_theta_grid[j][i]**2             for i in range(x_size)] for j in range(r_size)]
+h0_grid         = [[C_p*T0                                              for i in range(x_size)] for j in range(r_size)]
+h0r_grid        = [[C_p*T0                                              for i in range(x_size)] for j in range(r_size)]
+h_grid          = [[h0r_grid[j][i]-V_sqrt_grid[j][i]/2                  for i in range(x_size)] for j in range(r_size)]
+p0_grid         = [[p0                                                  for i in range(x_size)] for j in range(r_size)]
+p0r_grid        = [[p0                                                  for i in range(x_size)] for j in range(r_size)]
+p0r_ideal_grid  = [[p0                                                  for i in range(x_size)] for j in range(r_size)]
+p_grid          = [[p0_grid[j][i]*(h_grid[j][i]/h0_grid[j][i])**e_gamma for i in range(x_size)] for j in range(r_size)]
+rho_grid        = [[rho                                                 for i in range(x_size)] for j in range(r_size)]
+s_grid          = [[0                                                   for i in range(x_size)] for j in range(r_size)]
+
+#Starting iteration and checking for convergence after each run
+iteration = 0
+while(True):
     #Storing values of previous run
+    iteration             = iteration+1
     psi_old_grid          = [[psi_grid[j][i] for i in range(x_size)] for j in range(r_size)]
     vorticity_old_grid    = [[vorticity_grid[j][i] for i in range(x_size)] for j in range(r_size)]
     rho_old_grid          = [[rho_grid[j][i] for i in range(x_size)] for j in range(r_size)]
 
     #Updating vorticity
     for i in x_pos:
-        for j in range(1, max(r_pos)):
+        for j in r_pos:
             m, n = [1, 1]
             if j == 0:
                 n = 0
@@ -162,7 +163,7 @@ for run in range(21):
     #Updating psi                            
     A_grid = [[0 for i in range(x_size)] for j in range(r_size)]
     B_grid = [[0 for i in range(x_size)] for j in range(r_size)]
-    for i in range(1, max(x_pos)+1):
+    for i in x_pos:
         for j in r_pos:
             k, l, m, n = [1, 1, 1, 1]
             #Special cases left edge
@@ -202,20 +203,20 @@ for run in range(21):
     for i in x_pos:
         for j in r_pos:
             k, l, m, n = [1, 1, 1, 1]
-            o, p       = [0, 0]
+            o, q       = [0, 0]
             if j == 0:
-                n, p = [0, 1]
+                n, q = [0, 1]
             elif j == max(r_pos):
-                m, p = [0, 1]
+                m, q = [0, 1]
             if i == 0:
                 l, o = [0, 1]
             elif i == max(x_pos):
                 k, o = [0, 1]
 
             C_r_grid[j][i] = -m_dot*(psi_grid[j][i+1*k]-psi_grid[j][i-1*l])/(2*math.pi*rho_grid[j][i]*r_cal(i, j)*(2-o)*element_dim)
-            C_x_grid[j][i] = m_dot*(psi_grid[j+1*m][i]-psi_grid[j-1*n][i])/(2*math.pi*rho_grid[j][i]*r_cal(i, j)*(2-p)*element_dim)
+            C_x_grid[j][i] = m_dot*(psi_grid[j+1*m][i]-psi_grid[j-1*n][i])/(2*math.pi*rho_grid[j][i]*r_cal(i, j)*(2-q)*element_dim)
             C_m_grid[j][i] = (C_x_grid[j][i]**2+C_r_grid[j][i]**2)**(1/2)
-                    
+                
     #Updating swirl
     for i in x_pos:
         for j in r_pos:
@@ -230,7 +231,6 @@ for run in range(21):
     for i in range(1, max(x_pos)+1):
         i_origin = i-1
         if i > x_hub_inlet and i <= x_hub_outlet:
-            i_origin = x_hub_inlet
             N_rotate = N
         else:
             N_rotate = 0
@@ -240,12 +240,12 @@ for run in range(21):
         for j in r_pos:
             while(True):
                 if psi_grid[j][i] <= psi_upper and psi_grid[j][i] >= psi_lower:
-                    break;
-                j_step = j_step+1;
+                    break
+                j_step = j_step+1
                 if j_step == max(r_pos):
                     print('Error')
                     j_step = j_step-1
-                    break;                
+                    break                            
                 psi_upper = psi_grid[j_step+1][i_origin]
                 psi_lower = psi_grid[j_step][i_origin]
             
@@ -261,15 +261,15 @@ for run in range(21):
             C_theta_origin          = C_theta_grid[j_step][i_origin]+frac*(C_theta_grid[j_step+1][i_origin]-C_theta_grid[j_step][i_origin])
             rC_theta_origin         = radius1*C_theta_origin
             V_theta_origin          = radius1*N_rotate-C_theta_origin
-            C_sqrt_origin           = C_m_origin**2+C_theta_origin**2
+            V_sqrt_origin           = C_m_origin**2+V_theta_origin**2
 
             h0r_origin              = h0_origin-N_rotate*rC_theta_origin+(N_rotate*radius1)**2/2
-            p0r_origin              = p0_origin*(h0r_origin/h0_origin)**(gamma/(gamma-1))
-            h_origin                = h0_origin-C_sqrt_origin/2
-            p_origin                = p0_origin*(h_origin/h0_origin)**(gamma/(gamma-1))
+            p0r_origin              = p0_origin*(h0r_origin/h0_origin)**e_gamma
+            h_origin                = h0r_origin-V_sqrt_origin/2
+            p_origin                = p0_origin*(h_origin/h0_origin)**e_gamma
 
             h0r_grid[j][i]          = h0r_origin-N_rotate**2*(radius1**2-radius2**2)/2
-            p0r_ideal_grid[j][i]    = p0r_origin*(h0r_grid[j][i]/h0r_origin)**(gamma/(gamma-1))
+            p0r_ideal_grid[j][i]    = p0r_origin*(h0r_grid[j][i]/h0r_origin)**e_gamma
             p0r_grid[j][i]          = p0r_ideal_grid[j][i]-loss_grid[j][i]*(p0r_origin-p_origin)
 
             if i <= x_hub_outlet:
@@ -283,9 +283,9 @@ for run in range(21):
             C_sqrt_grid[j][i]       = C_m_grid[j][i]**2+C_theta_grid[j][i]**2
             h_grid[j][i]            = h0r_grid[j][i]-V_sqrt_grid[j][i]/2
             h0_grid[j][i]           = h_grid[j][i]+C_sqrt_grid[j][i]/2
-            p0_grid[j][i]           = p0r_grid[j][i]*(h0_grid[j][i]/h0r_grid[j][i])**(gamma/(gamma-1))
-            T_grid[j][i]            = (h0_grid[j][i]-C_sqrt_grid[j][i])/(2*C_p)
-            p_grid[j][i]            = p0_grid[j][i]*(h_grid[j][i]/h0_grid[j][i])**(gamma/(gamma-1))
+            p0_grid[j][i]           = p0r_grid[j][i]*(h0_grid[j][i]/h0r_grid[j][i])**e_gamma
+            T_grid[j][i]            = h_grid[j][i]/C_p
+            p_grid[j][i]            = p0_grid[j][i]*(h_grid[j][i]/h0_grid[j][i])**e_gamma
             rho_grid[j][i]          = C_p*p_grid[j][i]/(R*h_grid[j][i])
             s_grid[j][i]            = s_origin+C_p*math.log(h0r_grid[j][i]/h0r_origin)-R*math.log(p0r_grid[j][i]/p0r_origin)
 
@@ -293,42 +293,47 @@ for run in range(21):
     psi_difference_grid          = [[0 for i in range(x_size)] for j in range(r_size)]
     vorticity_difference_grid    = [[0 for i in range(x_size)] for j in range(r_size)]
     rho_difference_grid          = [[0 for i in range(x_size)] for j in range(r_size)]
+
     for i in x_pos:
         for j in r_pos:
             psi_difference_grid[j][i]       = abs(psi_grid[j][i] - psi_old_grid[j][i])
             vorticity_difference_grid[j][i] = abs(vorticity_grid[j][i] - vorticity_old_grid[j][i])   
             rho_difference_grid[j][i]       = abs(rho_grid[j][i] - rho_old_grid[j][i])
-    delta_psi       = max(max(psi_difference_grid))
-    delta_vorticity = max(max(vorticity_difference_grid))
-    delta_rho       = max(max(rho_difference_grid))
-    print(delta_psi, delta_vorticity, delta_rho)
+    delta_psi       = np.amax(psi_difference_grid)
+    delta_vorticity = np.amax(vorticity_difference_grid)
+    delta_rho       = np.amax(rho_difference_grid)
+
+    print("Iteration: ", iteration, "Delta Psi:", delta_psi, "Delta Vorticity:", delta_vorticity, "Delta Rho:", delta_rho)
+    if delta_psi < 0.00001 and delta_vorticity < 0.01 and delta_rho < 0.001 and iteration > 50:
+        break
 
 #Calculating the angles
 for i in x_pos:
     for j in r_pos:
         V_theta_grid[j][i]      = N*r_cal(i, j)-C_theta_grid[j][i]
-        alpha_grid[j][i]        = math.atan(C_theta_grid[j][i]/C_x_grid[j][i])
-        beta_grid[j][i]         = math.atan(V_theta_grid[j][i]/C_m_grid[j][i])   
+        alpha_grid[j][i]        = (180/math.pi)*math.atan(C_theta_grid[j][i]/C_x_grid[j][i])
+        beta_grid[j][i]         = (180/math.pi)*math.atan(V_theta_grid[j][i]/C_m_grid[j][i])   
          
 #Plotting
 A = psi_grid
 B = vorticity_grid
 C = rho_grid
 D = C_theta_grid
-E = h0_grid
-F = s_grid
+E = C_x_grid
+F = C_r_grid
 G = beta_grid
 H = alpha_grid
 
 fig, ax = plt.subplots(8, 1, figsize=(17,9))
-graph0 = ax[0].matshow(A, origin='lower', vmin=min(min(A)), vmax=max(max(A)))
-graph1 = ax[1].matshow(B, origin='lower', vmin=min(min(B)), vmax=max(max(B)))
-graph2 = ax[2].matshow(C, origin='lower', vmin=min(min(C)), vmax=max(max(C)))
-graph3 = ax[3].matshow(D, origin='lower', vmin=min(min(D)), vmax=max(max(D)))
-graph4 = ax[4].matshow(E, origin='lower', vmin=min(min(E)), vmax=max(max(E)))
-graph5 = ax[5].matshow(F, origin='lower', vmin=min(min(F)), vmax=max(max(F)))
-graph6 = ax[6].matshow(G, origin='lower', vmin=min(min(G)), vmax=max(max(G)))
-graph7 = ax[7].matshow(H, origin='lower', vmin=min(min(H)), vmax=max(max(H)))
+fig.tight_layout()
+graph0 = ax[0].matshow(A, origin='lower', vmin=np.amin(A), vmax=np.amax(A))
+graph1 = ax[1].matshow(B, origin='lower', vmin=np.amin(B), vmax=np.amax(B))
+graph2 = ax[2].matshow(C, origin='lower', vmin=np.amin(C), vmax=np.amax(C))
+graph3 = ax[3].matshow(D, origin='lower', vmin=np.amin(D), vmax=np.amax(D))
+graph4 = ax[4].matshow(E, origin='lower', vmin=np.amin(E), vmax=np.amax(E))
+graph5 = ax[5].matshow(F, origin='lower', vmin=np.amin(F), vmax=np.amax(F))
+graph6 = ax[6].matshow(G, origin='lower', vmin=np.amin(G), vmax=np.amax(G))
+graph7 = ax[7].matshow(H, origin='lower', vmin=np.amin(H), vmax=np.amax(H))
 
 fig.colorbar(graph0, ax=ax[0])
 fig.colorbar(graph1, ax=ax[1])
@@ -340,13 +345,13 @@ fig.colorbar(graph6, ax=ax[6])
 fig.colorbar(graph7, ax=ax[7])
 
 ax[0].set_title('Stream function')
-ax[1].set_title('Vorticity')
-ax[2].set_title('Density')
-ax[3].set_title('C_theta')
-ax[4].set_title('Total enthalpy')
-ax[5].set_title('Entropy')
-ax[6].set_title('Beta')
-ax[7].set_title('Alpha')
+ax[1].set_title('Vorticity (rad/kg.s)')
+ax[2].set_title('Density (kg/m3)')
+ax[3].set_title('C_theta (m/s)')
+ax[4].set_title('C_x (m/s)')
+ax[5].set_title('C_r (m/s)')
+ax[6].set_title('Beta (degree)')
+ax[7].set_title('Alpha (degree)')
 
 
 ax[0].xaxis.set_ticks_position('bottom')
